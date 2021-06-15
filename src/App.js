e45ef4d27c980e58d93bb58382394653c0bbdb13
Samuel Bouye <bouyesamuel@gmail.com>
2021-06-15 12:14:12 +0000

    edit works

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
 
   const [input, setInput] = useState('')
   const [todos, setTodos] = useState([])
+  const [isEdit, setIsEdit] = useState(false)
+  const [edit, setEdit] = useState('')
+
 
   function handleChange(e){
     setInput(e.target.value)
@@ -22,12 +25,39 @@ function App() {
   function deleteHandle(id) {
     setTodos(todos.filter((i) => ((i.id) !== id)))
   }
+
+
+
+
+
+  function editHandle(id){
+    let findTodoIndex = todos.findIndex(i => i.id === id )
+    let oldTodos = [...todos]
+    // setInput(okI[findI].todo)
+
+    // let editTodo = 
+    // setTodos((todos) => [editTodo])
+    // console.log('clicker edited',okI[findI])
+    if(oldTodos[findTodoIndex].id === id){
+      setIsEdit(true)
+    }
+  }
+
+  function saveEditHandle(id, edited){
+    let findTodoIndex = todos.findIndex(i => i.id === id )
+    let oldTodos = [...todos]
+    oldTodos[findTodoIndex] = {id: oldTodos[findTodoIndex].id ,todo: edited, status: oldTodos[findTodoIndex].status}
+    setTodos(oldTodos)
+    setIsEdit(false)
+    console.log(todos)
+  }
+
   return <div>
     <div>
-      <h1>MY TODO</h1>
+      <h1>MY TODOS</h1>
       <input onChange={handleChange} type="text" value={input} placeholder="Add new todo" />
       <button onClick={addNewTodo}>Add</button>
-      {todos.map((todo, i) => <Todo delete={deleteHandle} todoInfo={todo} key={todo.id} />)}
+      {todos.map((todo, i) => <Todo edit={editHandle} save={saveEditHandle} delete={deleteHandle} todoInfo={todo} isEdit={isEdit} key={todo.id} />)}
       
     </div>
   </div>
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,25 @@
+import {useState} from 'react'
+
 function Todo(props) {
+
+  const [input, setInput] = useState(props.todoInfo.todo)
+
+  function handleChangeEdit(e){
+    setInput(e.target.value)
+  }
+  
+  function handleSave(){
+    props.save(props.todoInfo.id, input)
+    console.log(input)
+  }
+
+
     return <div className="card">
       Todo {props.todoInfo.length}
-    <h2>{props.todoInfo.todo}</h2>
+    {props.isEdit ? <input onChange={handleChangeEdit} type="text" value={input}/> : <h2>{props.todoInfo.todo}</h2>}
     <div className="actions">
+
+      {!props.isEdit ? <button onClick={() => props.edit(props.todoInfo.id)}>Edit</button> : <button onClick={handleSave}>Save</button> }
       <button onClick={() => props.delete(props.todoInfo.id)} className="btn">Delete</button>
     </div>
 </div>
